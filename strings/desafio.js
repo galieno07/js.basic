59ea848c2b0b9bc0b82df421cdeaede604fe873e
gali@gali <gali@gali>
2026-03-13 16:54:49 -0300

    buscando o js

---
+++ strings/desafio.js
@@ -0,0 +1,7 @@
+let frase = "Aprender JavaScript é divertido!"
+console.log(frase.length)
+
+let texto ="JavaScript é incrivel"
+
+console.log(texto.toUpperCase('JavaScript é incrivel'))
+
---
+++ strings/strings.js
@@ -0,0 +1,41 @@
+
+  nome = 'Galieno Francisco Domingos Antonio'
+
+console.log("Imprimindo o tamanho de uma string: ")
+console.log(nome.length) 
+
+console.log(`O nome possui ${nome} possui ${nome.length} caracteres`)
+console.log(`O nome possui ${nome} possui ${nome.length} caracteres`)
+
+console.log("Posição de cada letra e")
+
+console.log(nome[0])
+console.log(nome[1])
+console.log(nome[1])
+console.log(nome[3])
+console.log(nome[4])
+console.log(nome[5])
+console.log(nome[6])
+console.log(nome[7])
+console.log(nome[8])
+console.log(nome[9])
+console.log(nome[10]) 
+
+let saudacao = "Olá, Mundo!";
+console.log(saudacao.toUpperCase()); // "OLÁ, MUNDO!"
+/* console.log(saudacao.toLowerCase()); // "olá, mundo!" 
+
+console.log(saudacao)
+
+saudacao = saudacao.toUpperCase()
+
+console.log(saudacao)
+
+let frase = "AprEnder JavaScript é divertido!";
+
+console.log("O primero n minusculo aparece no indice " + frase.indexOf('e'))
+console.log("O javascript minusculo aparece no indice " + frase.indexOf('JavaScript'))
+
+let nome = "Desenvolvedor Web";
+let parte = nome.slice(0, 9); // "Desenvolvedo"
+console.log(parte);
