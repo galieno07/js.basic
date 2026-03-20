df7a11f1e24de3ddd9cef82a31934467b631a0b0
gali@gali <gali@gali>
2026-03-20 13:28:53 -0300

    new

--- loops/exercicio1.js
+++ loops/exercicio1.js
@@ -3,5 +3,9 @@ const prompt = require('prompt-sync')()
 numero = parseFloat(prompt("Digite um numero para ver a tabuada: "));
 
  for (let i = 1; i <= 10; i++){
-    console.log(numero + " X " + i + " = " + (numero*i))
+   console.log(numero + " X " + i + " = " + (numero*i))
+ }
+
+ for (let i = 10; i >= 1; i--){
+   console.log(i)
  }
